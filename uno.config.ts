b9ace947ca0f4dcac36deecd20ef2c973795a1b1
colemeyer <cmeyer@jhb.dvt.co.za>
2023-09-08 15:29:41 +0200

    Feat: Refining Styling

--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,11 @@
 // uno.config.ts
-import { defineConfig } from 'unocss'
+import {defineConfig, presetAttributify, presetTypography} from 'unocss'
 import presetUno from '@unocss/preset-uno'
 
 export default defineConfig({
     presets: [
+        presetAttributify(),
         presetUno(),
+        presetTypography(),
     ],
 })
