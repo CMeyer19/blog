fa53693f953395281778a460b2d25a241614f8de
colemeyer <cmeyer@jhb.dvt.co.za>
2023-09-08 14:49:05 +0200

    Feat: Completing uno implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,13 @@
 import {defineConfig} from 'astro/config';
 import UnoCSS from 'unocss/astro'
 import preact from "@astrojs/preact";
-import {presetWind} from "@unocss/preset-wind";
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [
         preact(),
         UnoCSS({
-            presets: [
-                presetWind(),
-                /* more presets */
-            ],
-            safelist: [
-                /* this you can use to exclude utilities from purge */
-            ],
+            injectReset: true // or a path to the reset file
         }),
     ]
 });
