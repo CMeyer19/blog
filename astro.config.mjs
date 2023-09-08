36c3ab897914c1aed12a5d91b40a7e0dee9c9804
colemeyer <cmeyer@jhb.dvt.co.za>
2023-09-08 08:21:01 +0200

    Feat: Adding unocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,22 @@
-import { defineConfig } from 'astro/config';
-
+import {defineConfig} from 'astro/config';
+import UnoCSS from 'unocss/astro'
 import preact from "@astrojs/preact";
+import {presetWind} from "@unocss/preset-wind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+    integrations: [
+        preact(),
+        UnoCSS({
+            presets: [
+                presetWind(),
+                /* more presets */
+            ],
+            safelist: [
+                /* this you can use to exclude utilities from purge */
+            ],
+        }),
+    ]
 });
+
+
